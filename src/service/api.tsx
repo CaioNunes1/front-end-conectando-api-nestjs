a812deda1d3a71513fcb6ae441dc5134a0ce650a
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2024-05-22 10:43:54 -0300

    error on url api

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'https://cadastro-front-end-com-backend-nestjs.netlify.app/',
+    baseURL: 'https://learning-api-nestjs.vercel.app/',
     //baseURL: 'http://localhost:3333/',
     withCredentials: true,
 });
