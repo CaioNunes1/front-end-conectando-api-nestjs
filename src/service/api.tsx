a87a6ed75969a5d63c4b289fa8c37d9a210ce157
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2024-05-23 21:30:49 -0300

    setting new url

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'https://learning-api-nestjs.vercel.app/',
+    baseURL: 'https://web-production-c1d5.up.railway.app/',
     //baseURL: 'http://localhost:3333/',
     withCredentials: true,
 });
