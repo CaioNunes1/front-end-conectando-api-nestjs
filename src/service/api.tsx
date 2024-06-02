a8025460d0ccda0ac01d4c8ee339b9129e644e3e
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2024-06-02 15:01:59 -0300

    teste com a api back no localhost

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'https://web-production-c1d5.up.railway.app/',
-    //baseURL: 'http://localhost:3333/',
+    //baseURL: 'https://web-production-c1d5.up.railway.app/',
+    baseURL: 'http://localhost:3333/',
     withCredentials: true,
 });
 
